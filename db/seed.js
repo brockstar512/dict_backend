70d3cf5b827f01d5bbffea361a08e598e60467d5
marshall brock <marshallbrock@gmail.com>
2020-05-19 22:00:45 -0500

    seeded

--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,39 @@
-const Model = reqiure('../models/words')
+const Word = require('../models/Word')
+const Definition = require('../models/Definition')
 const mongoose = require('./connection')
 
 
-Model.Word.deleteMany().then(()=> Model.Word.create({
+// Word.deleteMany().then(()=> Model.Word.create({
 
-    "definitions": [
-                {
+//     "definitions": [
+//                 {
 
-                    "def": "the occurrence and development of events by chance in a happy or beneficial way.",
-                    "partOfSpeech": "noun",
-                    "sentence": "a fortunate stroke of serendipity"
-                }
-            ],
-    "word": "serendipity",
-    "notes": "It's similar to coincidence",
-}).then(json => {console.log(json), process.exit()}))
+//                     "def": "the occurrence and development of events by chance in a happy or beneficial way.",
+//                     "partOfSpeech": "noun",
+//                     "sentence": "a fortunate stroke of serendipity"
+//                 }
+//             ],
+//     "word": "serendipity",
+//     "notes": "It's similar to coincidence",
+// }).then(json => {console.log(json), process.exit()}))
 
-
+Word.find({}).remove(() => {
+    Definition.find({}).remove(() => {
+let example = Word.create({
+    word: "serendipity",
+    notes: "It's similar to coincidence"
+  }).then(word => {
+    Promise.all([
+      Definition.create({
+        def: "the occurrence and development of events by chance in a happy or beneficial way.",
+        partOfSpeech: "noun",
+        sentence: "a fortunate stroke of serendipity"
+      }).then(def => {
+        word.definitions.push(def)
+      })
+    ]).then(() => {
+        word.save()
+    }).then(console.log("seeded"),()=> process.exit())
+        })
+    })
+})
