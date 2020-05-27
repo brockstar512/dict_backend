4ad33bbf9de1dfb3e8bd0c4460c96bc23a6534c5
marshall brock <marshallbrock@gmail.com>
2020-05-26 23:54:12 -0500

    no arry this time

--- db/seed.js
+++ db/seed.js
@@ -28,7 +28,7 @@ let example = Word.create({
   }).then(word => {
     Promise.all([
       Definition.create({
-        def: ["the occurrence and development of events by chance in a happy or beneficial way."],
+        def: "the occurrence and development of events by chance in a happy or beneficial way.",
         partOfSpeech: "noun",
         sentence: "a fortunate stroke of serendipity"
       }).then(def => {
@@ -40,3 +40,4 @@ let example = Word.create({
         })
     })
 })
+// def: ["the occurrence and development of events by chance in a happy or beneficial way."]
